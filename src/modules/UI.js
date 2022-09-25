032eceb92c91d264246963fde9e11a5d7e7f4f7c
unknown <ogabr2020@gmail.com>
2022-09-24 20:55:12 -0500

    Finally completed this beefy ass project

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,6 +1,6 @@
 import create_new_project from "./project.js"
 import createNewTask from "./todo.js"
-
+import { dom_manipulate, render_task_to_page, render_project_to_page, make_new_project_active, create_task_container, delete_task } from "./displayController.js"
 
 function UI () {
     /*--------ARTICLE LINKS----------*/
@@ -18,120 +18,24 @@ function UI () {
     const submit_todo_button = document.querySelector('#submit_todo')
     const exit_todo_button = document.querySelector('#exit-todo-form') 
 
-    const results = document.querySelector('.results')
 
-    /*----------MASTER CONTAINER DISPLAYING PROJECTS--------*/
-    const master_container = document.querySelector('.master-container')
-    let project_items = document.querySelectorAll('.project-item')
-    let project_list_html = document.querySelector('.project-list')
+
+
 
     /******* LIST TO RENDER ALL THE NEW PROJECTS ********/
     const list_of_projects = []
 
 
-    function create_task_container(new_task){
-        let task = new_task
-        const todo_list = document.querySelector('.todo-list')
-        const todo_div = document.createElement('div')
-        todo_div.className = 'todo'
-        const todo_header_div = document.createElement('div')
-        todo_header_div.className = 'todo-header'
-        const h4 = document.createElement('h4')
-        h4.textContent = task.title
-        const p = document.createElement('p')
-        p.textContent = task.date
-        todo_header_div.appendChild(h4)
-        todo_header_div.appendChild(p)
-    
-        const todo_content_div = document.createElement('div')
-        todo_content_div.className = 'todo-content'
-        const p_todo_content = document.createElement('p')
-        p_todo_content.id = 'description'
-        p_todo_content.textContent = task.description
-        todo_content_div.appendChild(p_todo_content)
-        todo_div.appendChild(todo_header_div)
-        todo_div.appendChild(todo_content_div)
-        todo_list.appendChild(todo_div)
-    
-       return 
-    }
-
-
-    function loop_through_projects(list){
-        let todo_list = document.querySelector('.todo-list')
-        let target_index = list_of_projects.map(project => project.name).indexOf(list)
-        let project = list_of_projects[target_index]
-        let list_of_tasks = project.getTasks()
-        if (list_of_tasks.length > 0){
-            list_of_tasks.forEach(task => create_task_container(task))
-        } else{
-            todo_list.innerHTML = ""
-        }
-        
-   
-    }
-    
-
-
-
-    function render_project_to_page(project){
-        let new_project = project
-        let project_title = project.name
-        let project_list = document.querySelector('.project-list')
-        let div = document.createElement('div')
-        div.className = 'project-item'
-        div.id = `${project_title}`
-        div.textContent = project_title
-        let child_div = document.createElement('div')
-        child_div.className = 'container'
-        div.appendChild(child_div)
-        project_list.appendChild(div)
-        child_div.textContent = new_project.getTasks()
-        return;
-    }
-
-
-
-    function render_todos(){
-        let todo_list = document.querySelector('.todo-list')
-        todo_list.innerHTML = ''
-        return;
-    }
-
-    function render_task_to_page(new_task){
-        let project_items = Array.from(document.querySelectorAll('.project-item'))
-        let project_id;
-        let addproject;
-        let grabbed_project;
-
-        project_items.forEach((project) => {
-            project.className === 'project-item active' ? project_id = project.id: project_id = ''
+    function delete_buttons(){
+        let delete_button = document.querySelectorAll('.delete-todo')
+        delete_button.forEach((del) => {
+            del.addEventListener('click', (e) => {
+                console.log(e)
+                let task = e.target.parentNode.id
+                delete_task(list_of_projects,task)
+            })
         })
-        console.log('Here is the project id', project_id)
-        addproject = list_of_projects.map(project => project.name).indexOf(project_id)
-        console.log('Here is the addproject', addproject)
-        if (addproject === -1){
-            console.log('Error, project not found.')
-        }
-        grabbed_project = list_of_projects[addproject]
-        console.log(grabbed_project)
-        grabbed_project.addTask(new_task)
-        console.log(`${JSON.stringify(grabbed_project.getTasks())}`)
-        list_of_projects.forEach((project) => {
-            console.log(`Here is the project: ${JSON.stringify(project)} and list of tasks: ${JSON.stringify(project.getTasks())}`)
-        })
-                          
- 
-        return;
-    }   
-    
-
-    
-    // project_items.forEach(project => {
-    //     project.addEventListener('click', (e) =>{
-    //         results.textContent = JSON.stringify(project)
-    //     })
-    // })
+    }
 
     new_project_form.addEventListener('click', (e) => {
         open_project_form.style.transform = "scale(1)"
@@ -154,8 +58,8 @@ function UI () {
         list_of_projects.push(new_project)
         console.log('Here is list of projects', list_of_projects)
         open_project_form.style.transform = "scale(0)"
-        dom_manipulate()
-        make_project_active()
+        dom_manipulate(list_of_projects)
+        make_new_project_active()
     })
 
     submit_todo_button.addEventListener('click', (e) => {
@@ -163,57 +67,13 @@ function UI () {
         let new_task = createNewTask(open_todo_form.title.value, 
                                     open_todo_form.date.value, 
                                     open_todo_form.description.value)
-        console.log(new_task)
-        render_task_to_page(new_task)
+        render_task_to_page(new_task,list_of_projects)
         create_task_container(new_task)
         open_todo_form.style.transform = "scale(0)"
+        delete_buttons()
     })
 
 
-
-    const dom_manipulate = () => {
-        const element = document.querySelectorAll('.project-item')
-        element.forEach(child => {
-            child.addEventListener('click', (e) => {
-                
-                dom_manipulate_active(child.classList)
-                console.log(e)
-                console.log(child.classList)
-                console.log(child.id)
-                // list_of_projects.forEach((project) => project.getTasks() )
-                render_todos()
-                loop_through_projects(child.id)
-                // if (target_project === false){
-                //     project_list_html.innerHTML = ''
-                // } else{
-                //     get_appropriate_project(target_project)
-                // }
-             
-
-                // results.innerHTML = JSON.stringify(child.textContent)
-
-            })
-        })
-    }
-
-    const make_project_active = () => {
-        const element = document.querySelector('.project-list')
-        let new_project = element.lastElementChild
-        dom_manipulate_active(new_project.classList)
-    }
-
-    const dom_manipulate_active = (target) => {
-        const element = document.querySelectorAll('.project-item')
-        
-        element.forEach(child => {
-            if (child.classList.contains("active")){
-                child.classList.remove('active') 
-            } 
-        })
-        target.toggle('active')
-    
-        return 
-    }
 
 }
 
--- src/modules/displayController.js
+++ src/modules/displayController.js
@@ -1,9 +1,11 @@
+import create_new_project from "./project"
 
     export function create_task_container(new_task){
         let task = new_task
         const todo_list = document.querySelector('.todo-list')
         const todo_div = document.createElement('div')
         todo_div.className = 'todo'
+        todo_div.id = task.title
         const todo_header_div = document.createElement('div')
         todo_header_div.className = 'todo-header'
         const h4 = document.createElement('h4')
@@ -18,9 +20,15 @@
         const p_todo_content = document.createElement('p')
         p_todo_content.id = 'description'
         p_todo_content.textContent = task.description
+        
+        const delete_todo_div = document.createElement('div')
+        delete_todo_div.className = 'delete-todo'
+        delete_todo_div.textContent = 'Delete'
+
         todo_content_div.appendChild(p_todo_content)
         todo_div.appendChild(todo_header_div)
         todo_div.appendChild(todo_content_div)
+        todo_div.appendChild(delete_todo_div)
         todo_list.appendChild(todo_div)
     
        return 
@@ -40,6 +48,15 @@
         
    
     }
+    export function verify_project(array, project_title){
+        let list_of_projects = array
+        let new_project_title = project_title
+        while (list_of_projects.forEach((project) === new_project_title)){
+           alert('Project already taken please try again.')
+        }
+        let new_project = create_new_project(new_project_title)
+        return new_project
+    }
 
     export function render_project_to_page(project){
         let new_project = project
@@ -57,17 +74,30 @@
         return;
     }
 
+    function delete_buttons(){
+        let delete_button = document.querySelectorAll('.delete-todo')
+        delete_button.forEach((del) => {
+            del.addEventListener('click', (e) => {
+                console.log(e)
+                let task = e.target.parentNode.id
+                delete_task(list_of_projects,task)
+            })
+        })
+    }
+
 
     export const render_task_to_page = (new_task, array) => {
         let list_of_projects = array
-        let project_items = Array.from(document.querySelectorAll('.project-item'))
+        let project_items = document.querySelector('.project-list')
+        let children = Array.from(project_items.children)
+        console.log('Here is the children from render_task_page: ', children)
         let project_id;
         let addproject;
         let grabbed_project;
-     
-        project_items.forEach((project) => {
-            project.className === 'project-item active' ? project_id = project.id: project_id = ''
-        })
+        let magicword = 'project-item active'
+        let decider = children.map((project) => project.className).indexOf(magicword)
+        project_id = children[decider]
+        project_id = project_id.textContent
         console.log('Here is the project id', project_id)
         addproject = list_of_projects.map(project => project.name).indexOf(project_id)
         console.log('Here is the addproject', addproject)
@@ -81,7 +111,7 @@
         list_of_projects.forEach((project) => {
             console.log(`Here is the project: ${JSON.stringify(project)} and list of tasks: ${JSON.stringify(project.getTasks())}`)
         })
-            
+        delete_buttons()
  
         return;
     }
@@ -97,7 +127,7 @@
             return;
         }
 
-       let loop_through_projects = () => {
+       let loop_through_projects = (list) => {
        
             let todo_list = document.querySelector('.todo-list')
             let target_index = list_of_projects.map(project => project.name).indexOf(list)
@@ -121,10 +151,12 @@
        
                 render_todos()
                 loop_through_projects(child.id)
+                
 
 
             })
         })
+        delete_buttons()
     }
 
     export const make_new_project_active = () => {
@@ -142,17 +174,41 @@
             } 
         })
         target.toggle('active')
-    
+        delete_buttons()
         return 
     }
 
+    export function delete_task(array,task){
+        console.log('executing delete_task')
+        let list_of_projects = array
+        console.log(list_of_projects)
+        let element = document.querySelectorAll('.project-item')
+        let target_project;
+        let target_task = task
+        console.log('This is the target task: ', target_task)
    
+        element.forEach((child) => {
+            if (child.classList.contains("active")){
+                target_project = child.id
+                console.log('Here is the target project: ', target_project)
+            }
+        })
 
+        list_of_projects.forEach((project) => {
+            if (project.name === target_project){
+                target_project = project
+                console.log('Found the actual project from list of projects! :) ', target_project)
+            }
+        })
+        console.log('Task before being deleted', target_project.getTasks())
+        target_project.deleteTask(target_task)
+        console.log('Task deleted', target_project.getTasks())
+        console.log(target_project)
+        const remove_todo = document.getElementById(`${target_task}`)
+        while(remove_todo.firstChild){
+            remove_todo.removeChild(remove_todo.firstChild)
+        }
+        remove_todo.remove()
+   
+    }
 
-
-
-
-
-
-
-// export { create_task_container, get_appropriate_project, loop_through_projects }
